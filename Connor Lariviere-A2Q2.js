29673f84f2a02f28f0428e01d50102fb1f6c94dc
clarivi <71781445+clarivi@users.noreply.github.com>
2023-02-27 12:16:42 -0500

    Create Connor Lariviere-A2Q2.js
    This sketch is a shape created using the vertex method in p5. The link to the sketch is here: https://editor.p5js.org/connorL/sketches/J3IBuddGl

---
+++ Connor Lariviere-A2Q2.js
@@ -0,0 +1,27 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(0);
+  fill("white")
+  beginShape();
+  vertex(200,50)
+  vertex(300,300)
+  vertex(50,150)
+  vertex(350,150)
+  vertex(100,300)
+  vertex(200,50)
+  vertex(200,240)
+  vertex(200,190)
+  vertex(300,300)
+  vertex(200,190)
+  vertex(100,300)
+  vertex(200,190)
+  vertex(50,150)
+  vertex(200,190)
+  vertex(350,150)
+  vertex(200,190)
+  vertex(200,50)
+  endShape();
+}
